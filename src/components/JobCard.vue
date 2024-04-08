6ba338ecdcf151b10bea8a0474756733dd75fdfc
Jan <58551692+skyZcoding@users.noreply.github.com>
2024-04-08 23:59:14 +0200

    Update JobCard.vue

--- src/components/JobCard.vue
+++ src/components/JobCard.vue
@@ -5,19 +5,20 @@
         <p class="text-gray-600">Wir suchen einen Junior Frontend Developer, der uns bei der Entwicklung von Webanwendungen unterstützt.</p>
 
         <div class="flex flex-row flex-wrap gap-4 mt-2">
-            <div class="px-2 py-[0.5px] bg-blue-500 bg-opacity-20 rounded-md flex flex-row items-center">
+            <div class="px-2 py-[0.5px] bg-blue-500 bg-opacity-20 rounded-full flex flex-row items-center">
                 <div class="rounded-full h-2 w-2 bg-blue-500 mr-2"></div>
                 <p>Fulltime</p>
             </div>
-            <div class="px-2 py-[0.5px] bg-green-500 bg-opacity-20 rounded-md flex flex-row items-center">
+            <div class="px-2 py-[0.5px] bg-green-500 bg-opacity-20 rounded-full flex flex-row items-center">
                 <div class="rounded-full h-2 w-2 bg-green-500 mr-2"></div>
-                <p>Design</p>
+                <p>Vue.js</p>
             </div>
-            <div class="px-2 py-[0.5px] bg-red-500 bg-opacity-20 rounded-md flex flex-row items-center">
+            <div class="px-2 py-[0.5px] bg-red-500 bg-opacity-20 rounded-full flex flex-row items-center">
                 <div class="rounded-full h-2 w-2 bg-red-500 mr-2"></div>
-                <p>Entwicklung</p>
+                <p>On Site</p>
             </div>
-            
         </div>
+
+        
     </div>
 </template>
